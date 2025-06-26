cce2dcb80620cf685f86b6c7d357ab4ef8606a93
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-06-26 16:51:22 +0530

    Added: Navbar and footer

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Welcome to front Page</div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-4xl font-bold'>Welcome to Earth</h1>
+      <p className='text-lg'>A Productivity App</p>
+    </div>
   )
 }
 
--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -4,7 +4,19 @@ type Props = {}
 
 const Navbar = (props: Props) => {
   return (
-    <div>This is Navbar</div>
+    <div className='flex items-center justify-center p-4'>
+        <div className='flex justify-between fixed top-5 bg-white/20 text-white w-[80%] px-10 py-2 rounded-full mx-auto items-center gap-4'>
+            {/* left */}
+            <div className='flex items-center gap-4'>
+                <h1 className='text-xl font-bold'>Earth</h1>
+            </div>
+            {/* right */}
+            <div className='flex items-center gap-4'>
+                <h1 className='text-xl font-bold'>Login</h1>
+            </div>
+
+        </div>
+    </div>
   )
 }
 
